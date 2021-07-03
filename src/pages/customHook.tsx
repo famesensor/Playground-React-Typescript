69b4aebff12c5116bdcfef240c147237ce5e1d7a
famesensor <sathit.ju@outlook.com>
2021-07-03 22:13:51 +0700

    [:hammer:]: update custom hook page

--- src/pages/customHook.tsx
+++ src/pages/customHook.tsx
@@ -7,12 +7,22 @@ interface Pokemon {
     url: string;
 }
 
-function CustomHook() {
+const CustomHook: React.FC = () => {
     const { data } = useFetchData<Pokemon[]>(
         `${process.env.REACT_APP_URL}pokemon?limit=10&offset=0`
     );
 
-    return <div></div>;
-}
+    const listItems = data?.map((res) => {
+        return <li key={res.url}>{res.name}</li>;
+    });
+
+    return (
+        <div>
+            <h2>Custom-Hook</h2>
+            <h4>Pokemon</h4>
+            <ul>{listItems}</ul>
+        </div>
+    );
+};
 
 export default CustomHook;
