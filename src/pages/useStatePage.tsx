a3a14abc45ec9f3fc4dffe5139841e5bc564d701
famesensor <sathit.ju@outlook.com>
2021-07-03 22:44:52 +0700

    [:hammer:]: update usestate page

---
+++ src/pages/useStatePage.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+type Props = { initialCount: number };
+
+const UseStatePage = ({ initialCount }: Props) => {
+    const [count, setCount] = useState(initialCount);
+
+    return (
+        <div>
+            <h2>UseState</h2>
+            <h4>Count: {count}</h4>
+            <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+                Increment
+            </button>
+            <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+                Decrement
+            </button>
+            <button onClick={() => setCount(initialCount)}>Reset</button>
+        </div>
+    );
+};
+
+export default UseStatePage;
