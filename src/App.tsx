67a93eea6fea11cc87a1c0b821a1a24fc7cb7b4c
famesensor <sathit.ju@outlook.com>
2021-07-03 22:33:06 +0700

    [:hammer:]: update style and add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
 import React from 'react';
-import './App.css';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 import CustomHook from './pages/customHook';
+import UseEffectPage from './pages/useEffectPage';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1
+        },
+        paper: {
+            padding: theme.spacing(2),
+            textAlign: 'center',
+            color: theme.palette.text.secondary
+        }
+    })
+);
 
 function App() {
+    const classes = useStyles();
+
     return (
-        <div className='App'>
-            <CustomHook />
-        </div>
+        <Container>
+            <div className={classes.root}>
+                <Grid container spacing={4}>
+                    <Grid item xs={6}>
+                        <Paper className={classes.paper}>
+                            <UseEffectPage />
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper className={classes.paper}>
+                            <CustomHook />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </div>
+        </Container>
     );
 }
 
