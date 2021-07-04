1ce1eaf8a29e06f96ad73f0dfcfaba3939f554a7
famesensor <sathit.ju@outlook.com>
2021-07-04 22:49:59 +0700

    [:hammer:]: add toggle button for context

---
+++ src/context/theme-button.tsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { ThemeContext, ThemeContextProps } from './theme-context';
+
+export const ToggleThemeButton: React.FC = () => {
+    const theme = useContext<ThemeContextProps>(ThemeContext);
+
+    return (
+        <button style={theme.theme} onClick={theme.toggleTheme}>
+            Toggle Switch
+        </button>
+    );
+
+    // return (
+    //     <ThemeContext.Consumer>
+    //         {({ theme, toggleTheme }) => (
+    //             <button style={theme} onClick={toggleTheme}>
+    //                 Toggle Swicth
+    //             </button>
+    //         )}
+    //     </ThemeContext.Consumer>
+    // );
+};
