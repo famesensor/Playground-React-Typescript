0a86cd21c10dad4e8fc51b30f5b0d493ea29377d
famesensor <sathit.ju@outlook.com>
2021-07-04 22:50:15 +0700

    [:hammer:]: context store

---
+++ src/context/theme-context.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export type Theme = React.CSSProperties;
+
+interface Themes {
+    dark: Theme;
+    light: Theme;
+}
+
+export const themes: Themes = {
+    dark: {
+        color: 'black',
+        backgroundColor: 'white'
+    },
+    light: {
+        color: 'white',
+        backgroundColor: 'black'
+    }
+};
+
+export type ThemeContextProps = { theme: Theme; toggleTheme?: () => void };
+
+export const ThemeContext = React.createContext<ThemeContextProps>({
+    theme: themes.light
+});
